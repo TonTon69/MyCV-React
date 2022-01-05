65d2ca72f3e65fbadd7bed3a7f3589cb75b28a07
TonTon69 <hoanghuynh0609@gmail.com>
2022-01-05 15:39:17 +0700

    update about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,12 +4,48 @@ import Heading from "../../components/Heading";
 export default function About() {
     return (
         <div className={styles.wrapper}>
-            <Heading
-                first={"w"}
-                second={"ho am I"}
-                bg={"linear-gradient(to right bottom, #ff6bcb, #ffa400)"}
-            />
-            <div></div>
+            <div className={styles.container}>
+                <Heading
+                    first={"w"}
+                    second={"ho am I"}
+                    bg={"linear-gradient(to right bottom, #ff6bcb, #ffa400)"}
+                />
+                <div className={styles.content}>
+                    <div className={styles.left}>
+                        <p>
+                            My real name is <strong>Hoang</strong>. I am
+                            currently studying at Hutech. I love to code with
+                            <strong> HTML CSS</strong> to make stunning UI and
+                            have good UX for website.
+                        </p>
+                        <p>
+                            4 words that "always" describe me: always
+                            <strong> smile</strong>, always
+                            <strong> gentle</strong>, always
+                            <strong> understanding</strong>, always
+                            <strong> help</strong>.
+                        </p>
+                        <p>
+                            I love to play <strong>chess</strong> and
+                            <strong> travel</strong>.
+                        </p>
+                        <a
+                            href="https://www.facebook.com/humiho.69"
+                            className={styles.leftLink}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <span>Follow me</span>
+                        </a>
+                    </div>
+                    <div className={styles.right}>
+                        <img
+                            src="https://scontent.fdad1-1.fna.fbcdn.net/v/t39.30808-6/p960x960/269852707_1607151509630038_6173876227435305148_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=730e14&_nc_ohc=6fzxhxK72AgAX9EBNKe&_nc_ht=scontent.fdad1-1.fna&oh=00_AT9KCpsnT9AiYCYXUTcyE0izBjr67IPWHilaW18bbSC7Ww&oe=61D96DEE"
+                            alt="hello"
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -5,23 +5,25 @@ import AstronautMoon from "../../assets/images/astronaut_moon.png";
 export default function Skills() {
     return (
         <div className={styles.wrapper}>
-            <Heading
-                first={"p"}
-                second={"owerful skills"}
-                bg={
-                    "linear-gradient(to right bottom,rgb(51, 202, 248),rgb(51, 137, 248))"
-                }
-            />
-            <div className={styles.content}>
-                <span>4</span>
-                <div className={styles.contentImg}>
-                    <img src={AstronautMoon} alt="skills" />
+            <div className={styles.container}>
+                <Heading
+                    first={"p"}
+                    second={"owerful skills"}
+                    bg={
+                        "linear-gradient(to right bottom,rgb(51, 202, 248),rgb(51, 137, 248))"
+                    }
+                />
+                <div className={styles.content}>
+                    <span>4</span>
+                    <div className={styles.contentImg}>
+                        <img src={AstronautMoon} alt="skills" />
+                    </div>
+                    <span>4</span>
                 </div>
-                <span>4</span>
+                <h3>
+                    skills <span>not</span> found 😱
+                </h3>
             </div>
-            <h3>
-                skills <span>not</span> found 😱
-            </h3>
         </div>
     );
 }
