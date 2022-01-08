a7abb31c30619eb93f46a5ba577c3dd02ae864ac
TonTon69 <hoanghuynh0609@gmail.com>
2022-01-08 21:11:39 +0700

    update route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
@@ -6,12 +6,10 @@ import Home from "./pages/Home";
 function App() {
     return (
         <div className="App">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
         </div>
     );
 }
